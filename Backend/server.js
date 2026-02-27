982ac9b30d19472f037c38a826f9029cf5b8ef19
nidhibansal3105 <dreamygirl3105nidhi@gmail.com>
2026-02-27 17:07:23 +0530

    profile photo

--- Backend/server.js
+++ Backend/server.js
@@ -4,7 +4,9 @@ require("dotenv").config();
 const authRoutes = require("./routes/authRoutes");
 const postRoutes=require("./routes/postRoutes");
 const userRoutes = require("./routes/userRoutes");
+const profileRouter = require('./routes/profile');
 const app = express();
+const path = require('path');
 
 // Middlewares
 app.use(cors());
@@ -15,7 +17,9 @@ app.use(express.json()); // Frontend se aane wale JSON data ko samajhne ke liye
 // Ab aapke sare auth routes http://localhost:5001/api/auth/... par milenge
 app.use("/api/auth", authRoutes);
 app.use("/api/posts", postRoutes);
-app.use("/api/users",userRoutes)
+app.use("/api/users",userRoutes);
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('/api/profile', profileRouter);
 // Ek basic route testing ke liye
 app.get("/", (req, res) => {
   res.send("Connecto Backend is running! 🚀");
